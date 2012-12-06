c1d07d8e82536466c12bd636c940d240042e1f48
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-12-06 04:13:02 -0300

    Creating a View Class

--- js/main.js
+++ js/main.js
@@ -1,20 +1,10 @@
 var app = {
 
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees){
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        this.store = new WebSqlStore(function(){
-            self.renderHomeView();
+        this.store = new WebSqlStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     showAlert: function(message, title){
@@ -23,11 +13,6 @@ var app = {
         }else{
             alert(title ? (title + ": " + message) : message);
         }
-    },
-
-    renderHomeView: function(){
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 };
 
