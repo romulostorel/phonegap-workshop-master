c7d5d243de7145434cb1c1258d3afe3d08d9ef67
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-12-06 12:25:06 -0300

    View Routing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = {
 
     registerEvents: function() {
+        $(window).on('hashchange', $.proxy(this.route, this));
         var self = this;
     // Check of browser supports touch events...
     if (document.documentElement.hasOwnProperty('ontouchstart')) {
@@ -22,11 +23,26 @@ var app = {
     }
 },
 
+route: function(){
+    var hash = window.location.hash;
+    if(!hash){
+        $('body').html(new HomeView(this.store).render().el);
+        return;
+    }
+    var match = hash.match(app.detailsURL);
+    if(match){
+        this.store.findById(Number(match[1]), function(employee){
+            $('body').html(new EmployeeView(employee).render().el);
+        });
+    }
+},
+
 initialize: function() {
     var self = this;
+    this.detailsURL = /^#employees\/(\d{1,})/;
     this.registerEvents();
     this.store = new WebSqlStore(function() {
-        $('body').html(new HomeView(self.store).render().el);
+        self.route();
     });
 },
 
