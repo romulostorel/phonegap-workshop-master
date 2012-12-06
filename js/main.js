683d4d148dded1763b02fabe315251650dc53def
Sandro Queiroz Jr <sandroqz@gmail.com>
2012-12-06 05:10:17 -0300

    Highlighting Tapped or Clicked UI Elements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
 var app = {
 
-    initialize: function() {
+    registerEvents: function() {
         var self = this;
-        this.store = new WebSqlStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
+    // Check of browser supports touch events...
+    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+        // ... if yes: register touch event listener to change the "selected" state of the item
+        $('body').on('touchstart', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
         });
-    },
-
-    showAlert: function(message, title){
-        if(navigator.notification){
-            navigator.notification.alert(message, null, title, 'OK');
-        }else{
-            alert(title ? (title + ": " + message) : message);
-        }
+        $('body').on('touchend', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    } else {
+        // ... if not: register mouse events instead
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
     }
+},
+
+initialize: function() {
+    var self = this;
+    this.registerEvents();
+    this.store = new WebSqlStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
+    });
+},
+
+showAlert: function(message, title){
+    if(navigator.notification){
+        navigator.notification.alert(message, null, title, 'OK');
+    }else{
+        alert(title ? (title + ": " + message) : message);
+    }
+}
 };
 
 app.initialize();
